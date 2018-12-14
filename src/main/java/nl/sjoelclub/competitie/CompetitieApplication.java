fac639995441385048a9fc8b698ef72f1d11ad13
Kevin Sweeren <sweerenk@gmail.com>
2018-12-14 19:48:08 +0100

    first commit

---
+++ src/main/java/nl/sjoelclub/competitie/CompetitieApplication.java
@@ -0,0 +1,14 @@
+package nl.sjoelclub.competitie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitieApplication.class, args);
+	}
+
+}
+
